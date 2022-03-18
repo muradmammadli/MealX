7030f55ddf785e2bf5260dc67a805b4fcad76d9f
Murad <murad.mammadli@yahoo.com>
2022-03-18 23:49:51 +0400

    c

--- app/src/main/java/com/example/foodx/MainActivity.java
+++ app/src/main/java/com/example/foodx/MainActivity.java
@@ -24,11 +24,10 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         Log.d("Drawable",String.valueOf(R.drawable.burger));
         databaseHelper = new DatabaseHelper(this);
-        new MealDao().addMeal(databaseHelper,"Yarpaq dolması","5 AZN","Qoyun əti, düyü, baş soğan, keşniş, şüyüd, nanə, tənək yarpağı",R.drawable.yarpaqdolmasi);
-        new MealDao().addMeal(databaseHelper,"3 baci dolması","6 AZN","badımcan, yaşıl bibəri, pomidor, duz,istiot",R.drawable.ucbacidolmasi);
-        new MealDao().addMeal(databaseHelper,"Plov","4 AZN","düyü, süd, duz, zəfəran, kişmiş, xurma, kərə yağı, bal, qaymaq",R.drawable.plov);
-        new MealDao().addMeal(databaseHelper,"Ləvəngi","12 AZN","toyuq, soğan, qoz ləpəsi, alça turşusu, duz, istiot",R.drawable.levengi);
-
+//        new MealDao().addMeal(databaseHelper,"Yarpaq dolması","5 AZN","Qoyun əti, düyü, baş soğan, keşniş, şüyüd, nanə, tənək yarpağı",R.drawable.yarpaqdolmasi);
+//        new MealDao().addMeal(databaseHelper,"3 baci dolması","6 AZN","badımcan, yaşıl bibəri, pomidor, duz,istiot",R.drawable.ucbacidolmasi);
+//        new MealDao().addMeal(databaseHelper,"Plov","4 AZN","düyü, süd, duz, zəfəran, kişmiş, xurma, kərə yağı, bal, qaymaq",R.drawable.plov);
+//        new MealDao().addMeal(databaseHelper,"Ləvəngi","12 AZN","toyuq, soğan, qoz ləpəsi, alça turşusu, duz, istiot",R.drawable.levengi);
 
 
         List<MealModel> modelArrayList = new MealDao().mealModelArrayList(databaseHelper);
